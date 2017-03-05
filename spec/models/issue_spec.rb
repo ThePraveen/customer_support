6e8f8ed8671e3f4ce2af9cbbd1db009b7bff50b2
Nitesh Kumar <nitesh9952@gmail.com>
2017-03-05 12:24:32 +0530

    test case refactoring for user model

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -12,17 +12,23 @@ RSpec.describe Issue, type: :model do
       @customer = FactoryGirl.create(:customer, user_id: @user.id)
     end
 
-    it "Check if issues creation throws validation" do
+    it "Check if issues creation throws validation error when title and description are blank" do
       @issue = Issue.create(customer_id: @customer.id)
       expect(@issue.errors.messages[:title]).to eq ["can't be blank"]
       expect(@issue.errors.messages[:description]).to eq ["can't be blank"]
+    end
+
+    it "Check if issues creation throws validation error when description is blank" do
       @issue = Issue.create(customer_id: @customer.id, title: "Not able to start")
       expect(@issue.errors.messages[:description]).to eq ["can't be blank"]
+    end
+
+    it "Check if issues creation throws validation error when title is blank" do
       @issue = Issue.create(customer_id: @customer.id, description: "It's very complicated")
       expect(@issue.errors.messages[:title]).to eq ["can't be blank"]
     end
 
-    it "Check if issues are getting created with correct values" do
+    it "Check if issues are getting created with correct values and correct status got assigned" do
       @issue = Issue.create(customer_id: @customer.id, title: "Not able to start", description: "It's very complicated")
       expect(@issue.title).to eq "Not able to start"
       expect(@issue.description).to eq "It's very complicated"
